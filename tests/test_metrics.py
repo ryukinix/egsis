0a5168d47a3bf4fbe94309156986e466ee848aff
Manoel V. Machado <manoel_vilela@engineer.com>
2023-10-01 19:37:06 -0300

    Add metrics module (#25)
    Closes #22

---
+++ tests/test_metrics.py
@@ -0,0 +1,45 @@
+import pytest
+import numpy as np
+
+
+from egsis import metrics
+
+
+@pytest.fixture
+def x():
+    return np.array(
+        [[0, 0, 1],
+         [0, 1, 0],
+         [1, 0, 0]]
+    )
+
+
+@pytest.fixture
+def y():
+    return np.array(
+        [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]]
+    )
+
+
+def test_pixel_accuracy(x, y):
+    assert metrics.pixel_accuracy(x, y) == pytest.approx(0.5555, 0.001)
+
+
+def test_iou(x, y):
+    assert metrics.iou(x, y) == 0.20
+
+
+def test_f1(x, y):
+    assert metrics.f1(x, y) == pytest.approx(0.11111, 0.001)
+
+
+def test_iou_raise_error_on_non_binarized_labels(x, y):
+    with pytest.raises(ValueError):
+        metrics.iou(x + 1, y + 1)
+
+
+def test_f1_raise_error_on_non_binarized_labels(x, y):
+    with pytest.raises(ValueError):
+        metrics.iou(x + 1, y + 1)
