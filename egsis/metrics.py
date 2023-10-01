a18f5c4473d2dc29ec2868e982adad5c35352fd5
Manoel Vilela <manoel.neto@neoway.com.br>
2023-10-01 19:30:35 -0300

    Add metrics module
    Closes #22

---
+++ egsis/metrics.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def _check_if_they_are_binarized(y_true: np.ndarray, y_pred: np.ndarray):
+    set_of_binary = {0, 1}
+    set_of_values = np.unique(np.concatenate([y_true, y_pred]))
+    if any(value not in set_of_binary for value in set_of_values):
+        raise ValueError("such metric cannot be used in non-binary labels")
+
+
+def pixel_accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    equal_pixels = (y_true == y_pred).sum()
+    total = y_true.size
+    acc = equal_pixels / total
+    return acc
+
+
+def iou(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    _check_if_they_are_binarized(y_true, y_pred)
+    intersection = y_true & y_pred
+    union = y_true | y_pred
+    return intersection.sum() / union.sum()
+
+
+def f1(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    _check_if_they_are_binarized(y_true, y_pred)
+    intersection = y_true & y_pred
+    total_size = y_true.size + y_pred.size
+    return (2 * intersection.sum()) / total_size
